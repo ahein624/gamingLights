faea57043cbaa7e6ebe22d463aa750e1acf72b60
Andrew Hein <99760611+ahein624@users.noreply.github.com>
2026-08-07 17:50:47 -0400

    Render WLED animation panel

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import AnimationPortal from './AnimationPanel.jsx'
 import './styles.css'
 import './enhancements.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <AnimationPortal />
   </React.StrictMode>,
 )
