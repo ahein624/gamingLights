df5bec080bba8b649aa7839681063c23000c803b
Andrew Hein <99760611+ahein624@users.noreply.github.com>
2026-08-09 17:58:15 -0400

    Register PWA and add preset studio

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import AnimationPortal from './AnimationPanel.jsx'
+import PresetStudioPortal from './PresetStudio.jsx'
 import './styles.css'
 import './enhancements.css'
+
+if ('serviceWorker' in navigator && import.meta.env.PROD) {
+  window.addEventListener('load', () => navigator.serviceWorker.register('/sw.js').catch(() => {}))
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
     <AnimationPortal />
+    <PresetStudioPortal />
   </React.StrictMode>,
 )
